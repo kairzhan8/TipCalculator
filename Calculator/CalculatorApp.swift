38cc942382c25e9811596f28c986b5c5dc8b222f
kairzhan <kkural@dar.io>
2022-07-02 15:57:19 +0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Kaiyrzhan Kural on 02.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
